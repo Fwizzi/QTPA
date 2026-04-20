eeff54eeeb282e660e4d34c896a65231be4d9507
Fwizzi <vincent.guerlach@gmail.com>
2026-04-20 13:17:28 +0200

    Update SHORT object with new abbreviations

--- js/observations.js
+++ js/observations.js
@@ -9,7 +9,9 @@ import { fmt, escapeHtml } from './utils.js';
 import { log } from './logger.js';
 
 /* ── Abréviations pour les boutons de l'écran de saisie des abréviations en mode match (supprimées le 19/04/2026) ── */
-const SHORT = { 
+const SHORT = { 'Exécution du Jet' : 'Exécution Jet', 'Reprise de dribble' : 'Reprise dribble', 
+   'Passage en Force' : 'Passage Force', 'Communication' : 'Comm.', 
+   'Gestion du sifflet' : "Sifflet'
    
 };
 function shortName(cat) { return SHORT[cat] || cat; }
