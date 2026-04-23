6a2d1fecb8ac01374680ff6842011464aa022652
Fwizzi <vincent.guerlach@gmail.com>
2026-04-23 09:55:28 +0200

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
 /* ═══ VERSION — Source unique de vérité pour la version de l'application ═ */
-export const APP_VERSION  = '1.4.1';
+export const APP_VERSION  = '1.4.0';
 export const APP_YEAR     = '2026';
 export const APP_AUTHOR   = 'Vincent Guerlach';
